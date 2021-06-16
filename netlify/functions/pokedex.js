aa60c389d71c9a647861f0cbd6c5718b49f5c90f
Ben Hong <ben@bencodezen.io>
2021-06-16 19:41:57 -0400

    add pokedex with vue cli scaffold

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch')
+
+exports.handler = async () => {
+  const pokedexResponse = await fetch('https://pokeapi.co/api/v2/pokedex/1')
+  const data = await pokedexResponse.json()
+
+  const originalPokedex = data.pokemon_entries.slice(0, 150)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(originalPokedex)
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <ul>
+    <li v-for="pokemon in pokemonList" :key="`pokemon-${pokemon.entry_number}`">
+      ({{ pokemon.entry_number }}): {{ pokemon.pokemon_species.name }}
+    </li>
+  </ul>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    pokemonList: {}
+  }),
+  async mounted() {
+    const response = await fetch('/.netlify/functions/pokedex')
+    const pokemonData = await response.json()
+
+    this.pokemonList = pokemonData
+  }
+}
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
